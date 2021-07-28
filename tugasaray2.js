7907e10afa24f039fc7e0b7012b05ada509e7af9
rifkiiawan <86697981+rifkiiawan@users.noreply.github.com>
2021-07-28 11:12:21 +0700

    Add files via upload

---
+++ tugasaray2.js
@@ -0,0 +1,5 @@
+var santri_pondok = ["","Wandi", "Aldi", "Acep", "Putra", "Hafif", "Syofan","Rahmad"]
+
+    for(var i = 1; i < santri_pondok.length; i++){
+        console.log(i+"."+" "+santri_pondok[i])
+    }
