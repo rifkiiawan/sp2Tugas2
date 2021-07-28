649ba859c43bd59ebee8eb813bd951dc98f3c665
rifkiiawan <86697981+rifkiiawan@users.noreply.github.com>
2021-07-28 11:09:17 +0700

    Add files via upload

---
+++ tugasaray1.js
@@ -0,0 +1,5 @@
+var reactNative = ['Hafif' ,' Dayat',' Wandi']
+var  reactJs    = ['Yusuf', 'Luthfi', 'Tompel']
+var JavaScriptDivision = reactNative.concat(reactJs)
+
+console.log("JavaScriptDivision = " + JavaScriptDivision);
