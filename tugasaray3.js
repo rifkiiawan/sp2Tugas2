bd325b23f6f6c66d2b142a1f70251064be70e0f0
rifkiiawan <86697981+rifkiiawan@users.noreply.github.com>
2021-07-28 11:14:54 +0700

    Add files via upload

---
+++ tugasaray3.js
@@ -0,0 +1,10 @@
+var nilai_santri = [5,9,6,7,9,8,10,7,8]
+
+var jumlah_nilai = 0;
+
+for(var n=0; n < nilai_santri.length; n++){
+    jumlah_nilai += nilai_santri[n]
+
+}
+console.log(jumlah_nilai)
+console.log(jumlah_nilai/nilai_santri.length)
